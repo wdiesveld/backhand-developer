b0c67fa44b97d11a507f63ff87fda4f1c65461d8
Wouter Diesveld <wouter@tinyqueries.com>
2023-02-27 15:47:46 +0100

    added query

---
+++ dbt/models/marts/latest_rankings.sql
@@ -0,0 +1,26 @@
+WITH
+
+rankings AS (
+    SELECT * FROM {{ ref('stg_rankings') }}
+),
+
+final AS (
+    SELECT
+        r.ranking_date,
+        r.player_rank,
+        r.player_name,
+        r.player_points
+    FROM
+        rankings AS r
+    WHERE
+        r.ranking_date = (
+            SELECT
+                MAX(r2.ranking_date)
+            FROM
+                rankings AS r2
+        )
+    ORDER BY
+        r.player_rank
+)
+
+SELECT * FROM final
