43bb9b86bd8cc947807a79db0dc94950886efa4c
Wouter Diesveld <wouter@tinyqueries.com>
2023-02-27 10:53:38 +0100

    added check query

---
+++ dbt/models/marts/number_ones.sql
@@ -0,0 +1,19 @@
+WITH 
+
+rankings AS (
+    SELECT * FROM {{ ref('stg_rankings') }}
+),
+
+final AS (
+    SELECT
+        ranking_date,
+        player_name
+    FROM
+        rankings
+    WHERE
+        player_rank = 1
+    ORDER BY
+        ranking_date
+)
+
+SELECT * FROM final
