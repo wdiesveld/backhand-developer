cda735b3151f5a7dc3927b31b8223dc3895556da
Wouter Diesveld <wouter@tinyqueries.com>
2023-02-27 15:25:20 +0100

    added currentdate

--- dbt/models/staging/stg_ranking_dates.sql
+++ dbt/models/staging/stg_ranking_dates.sql
@@ -5,6 +5,6 @@
 {{ dbt_utils.date_spine(
     datepart="week",
     start_date="cast('2015-01-05' as date)",
-    end_date="cast('2022-12-26' as date)"
+    end_date="cast(DATEADD(day, 7, CURRENT_DATE()) as date)"
    )
 }}
