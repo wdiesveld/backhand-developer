cc06ee8c0e27fbd4b34e84dd2d15c0ab17971055
Wouter Diesveld <wouter@tinyqueries.com>
2023-02-27 08:41:14 +0100

    fix

--- dbt/macros/load_ranking_raw.sql
+++ dbt/macros/load_ranking_raw.sql
@@ -1,9 +1,6 @@
 {% macro load_ranking_raw(ranking_date) %}
 
-{% set date_str = ranking_date|string %}
-{% set date_sql = date_str[:4] ~ '-' ~ date_str[4:6] ~ '-' ~ date_str[6:8] %}
-
-{{ log(msg="Loading ranking data from " ~ date_sql, info=True) }}
+{{ log(msg="Loading ranking data from " ~ ranking_date, info=True) }}
 
 {% set query1 %}
 put file://C:\projects\backhand-developer\data\rankings\rankings-{{ ranking_date }}.json @BACKHAND_DEVELOPER.DEV.RANKING_STAGE;
@@ -13,7 +10,7 @@ put file://C:\projects\backhand-developer\data\rankings\rankings-{{ ranking_date
 INSERT INTO
     dev.rankings_raw (date, rankings)
         SELECT
-            '{{ date_sql }}',
+            '{{ ranking_date }}',
             $1
         FROM
             @BACKHAND_DEVELOPER.DEV.RANKING_STAGE/rankings-{{ ranking_date }}.json
