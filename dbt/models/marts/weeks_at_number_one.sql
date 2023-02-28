9605affb09a90d21dfe6c6bd02d27bf2061cd80e
Wouter Diesveld <wouter@tinyqueries.com>
2023-02-28 13:48:48 +0100

    added query

---
+++ dbt/models/marts/weeks_at_number_one.sql
@@ -0,0 +1,22 @@
+WITH
+
+rankings AS (
+    SELECT * from {{ ref('stg_rankings') }}
+),
+
+final AS (
+    SELECT
+        player_name,
+        COUNT(*) AS weeks_at_number_one
+    FROM
+        rankings
+    WHERE
+        player_rank = 1
+    GROUP BY
+        player_id,
+        player_name
+    ORDER BY
+        2 DESC
+)
+
+SELECT * FROM final
