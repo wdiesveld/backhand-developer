56292458cb98e1683fd89bb113c45a649020d065
Wouter Diesveld <wouter@tinyqueries.com>
2023-02-28 13:09:54 +0100

    set date

--- dbt/models/staging/stg_ranking_dates.sql
+++ dbt/models/staging/stg_ranking_dates.sql
@@ -4,7 +4,7 @@
 
 {{ dbt_utils.date_spine(
     datepart="week",
-    start_date="cast('2015-01-05' as date)",
+    start_date="cast('2012-01-02' as date)",
     end_date="cast(DATEADD(day, 7, CURRENT_DATE()) as date)"
    )
 }}
