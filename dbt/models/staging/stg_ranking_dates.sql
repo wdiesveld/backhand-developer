34eca88a27eb57e1327b6bd080ce42d6d23c372e
Wouter Diesveld <wouter@tinyqueries.com>
2023-03-01 08:07:36 +0100

    added var

--- dbt/models/staging/stg_ranking_dates.sql
+++ dbt/models/staging/stg_ranking_dates.sql
@@ -4,7 +4,7 @@
 
 {{ dbt_utils.date_spine(
     datepart="week",
-    start_date="cast('2012-01-02' as date)",
+    start_date="cast('" ~ var('start_date') ~ "' as date)",
     end_date="cast(DATEADD(day, 7, CURRENT_DATE()) as date)"
    )
 }}
