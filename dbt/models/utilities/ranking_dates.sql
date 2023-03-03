01a1a74655c947ae8063123b1b756dc6255709fd
Wouter Diesveld <wouter@tinyqueries.com>
2023-03-03 11:16:01 +0100

    added filter for omitted ranking dates

--- dbt/models/utilities/ranking_dates.sql
+++ dbt/models/utilities/ranking_dates.sql
@@ -2,9 +2,30 @@
     config(materialized="table")
 }}
 
-{{ dbt_utils.date_spine(
-    datepart="week",
-    start_date="cast('" ~ var('start_date') ~ "' as date)",
-    end_date="cast(DATEADD(day, 7, CURRENT_DATE()) as date)"
-   )
-}}
+WITH
+
+omitted AS (
+    SELECT * FROM {{ ref('ranking_dates_omitted') }}
+),
+
+date_spine AS (
+    {{ dbt_utils.date_spine(
+        datepart="week",
+        start_date="cast('" ~ var('start_date') ~ "' as date)",
+        end_date="cast(DATEADD(day, 7, CURRENT_DATE()) as date)"
+    )
+    }}
+),
+
+final AS (
+    SELECT
+        *
+    FROM
+        date_spine
+    WHERE
+        date_week NOT IN (
+            SELECT ranking_date_omit from omitted
+        )
+)
+
+SELECT * FROM final
