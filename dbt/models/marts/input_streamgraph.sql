1e0e5e1255abb8df4507c0525588706c2e157b60
Wouter Diesveld <wouter@tinyqueries.com>
2023-03-07 11:19:58 +0100

    refactor to json ouput

--- dbt/models/marts/input_streamgraph.sql
+++ dbt/models/marts/input_streamgraph.sql
@@ -6,16 +6,23 @@ rankings AS (
 
 final AS (
     SELECT
-        ranking_date,
-        player_id,
-        player_name,
-        player_rank,
-        player_points_normalized
+        OBJECT_CONSTRUCT(
+            'ranking_date', ranking_date,
+            'rankings', ARRAY_AGG(
+                OBJECT_CONSTRUCT(
+                    'player_id', player_id,
+                    'player_name', player_name,
+                    'player_rank', player_rank,
+                    'player_points_normalized', player_points_normalized
+                )
+            ) WITHIN GROUP (ORDER BY player_id)
+        )
     FROM
         rankings
+    GROUP BY
+        ranking_date
     ORDER BY
-        ranking_date,
-        player_id
+        ranking_date
 )
 
 SELECT * FROM final
