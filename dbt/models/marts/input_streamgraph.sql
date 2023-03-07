da427ec6fefa713f1f54fd3e37623040738386ad
Wouter Diesveld <wouter@tinyqueries.com>
2023-03-08 00:15:27 +0100

    to one array

--- dbt/models/marts/input_streamgraph.sql
+++ dbt/models/marts/input_streamgraph.sql
@@ -4,25 +4,39 @@ rankings AS (
     SELECT * FROM {{ ref('int_rankings_normalized') }}
 ),
 
-final AS (
+semi_final AS (
     SELECT
-        OBJECT_CONSTRUCT(
-            'ranking_date', ranking_date,
-            'rankings', ARRAY_AGG(
-                OBJECT_CONSTRUCT(
-                    'player_id', player_id,
-                    'player_name', player_name,
-                    'player_rank', player_rank,
-                    'player_points_normalized', player_points_normalized
-                )
-            ) WITHIN GROUP (ORDER BY player_id)
-        )
+        ranking_date,
+        ARRAY_AGG(
+            OBJECT_CONSTRUCT(
+                'player_id', player_id,
+                'player_name', player_name,
+                'player_rank', player_rank,
+                'player_points_normalized', player_points_normalized
+            )
+        ) WITHIN GROUP (
+            ORDER BY player_id
+        ) AS rankings
     FROM
         rankings
     GROUP BY
         ranking_date
     ORDER BY
         ranking_date
+),
+
+final AS (
+    SELECT
+        ARRAY_AGG(
+            OBJECT_CONSTRUCT(
+                'ranking_date', ranking_date,
+                'rankings', rankings
+            )
+        ) WITHIN GROUP (
+            ORDER BY ranking_date
+        ) AS ranking_data
+    FROM
+        semi_final
 )
 
 SELECT * FROM final
